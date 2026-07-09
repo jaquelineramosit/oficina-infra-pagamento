a17f0efdec3620b7f6a185d5d99afded1285335c
Jaqueline Ramos da Silva <jaquelineramosit@hotmail.com>
2026-07-09 20:08:23 -0300

    correcao retirada referencias lambda

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -32,3 +32,18 @@ output "lambda_event_source_mapping_uuid" {
   description = "UUID do event source mapping entre a fila SQS e a Lambda."
   value       = aws_lambda_event_source_mapping.payment_queue.uuid
 }
+
+output "table_name" {
+  description = "Nome da tabela DynamoDB."
+  value       = aws_dynamodb_table.main.name
+}
+
+output "table_arn" {
+  description = "ARN da tabela DynamoDB."
+  value       = aws_dynamodb_table.main.arn
+}
+
+output "database_name" {
+  description = "Nome logico do banco de dados."
+  value       = var.database_name
+}
