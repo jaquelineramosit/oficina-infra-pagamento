d96d322b59c4aba1a20d577abbbecd719505c800
Jaqueline Ramos da Silva <jaquelineramosit@hotmail.com>
2026-07-09 20:26:50 -0300

    limpando codigo apenas sqs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -27,23 +27,3 @@ output "dlq_url" {
   description = "URL da DLQ."
   value       = aws_sqs_queue.dlq.url
 }
-
-output "lambda_event_source_mapping_uuid" {
-  description = "UUID do event source mapping entre a fila SQS e a Lambda."
-  value       = aws_lambda_event_source_mapping.payment_queue.uuid
-}
-
-output "table_name" {
-  description = "Nome da tabela DynamoDB."
-  value       = aws_dynamodb_table.main.name
-}
-
-output "table_arn" {
-  description = "ARN da tabela DynamoDB."
-  value       = aws_dynamodb_table.main.arn
-}
-
-output "database_name" {
-  description = "Nome logico do banco de dados."
-  value       = var.database_name
-}
