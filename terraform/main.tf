dc6536704f03306ef685d6dc0b1264bd33422503
Jaqueline Ramos da Silva <jaquelineramosit@hotmail.com>
2026-07-10 09:48:47 -0300

    inclusao de labrole

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,6 +27,7 @@ resource "aws_sqs_queue" "dlq" {
 
 resource "aws_sqs_queue" "main" {
   name                       = var.queue_name
+  role_arn                   = var.labRole
   delay_seconds              = var.delay_seconds
   max_message_size           = var.max_message_size
   message_retention_seconds  = var.message_retention_seconds
